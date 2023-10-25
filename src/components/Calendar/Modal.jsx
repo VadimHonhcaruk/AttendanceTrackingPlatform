dea325f37118ee33986545c146bd2c6ffd0b65a8
Vadim Honcharuk <vadimhonc@gmail.com>
2023-10-25 13:31:05 +0300

    note bug fix, modal window fix

--- src/components/Calendar/Modal.jsx
+++ src/components/Calendar/Modal.jsx
@@ -15,9 +15,14 @@ export default function Modal({ groupId, choosenDay, setModalVision }) {
 
         async function fetchDataNote() {
             if (groupId) {
-                const response = await getNote(groupId, choosenDay, PATH, TOKEN, AUTH);
-                const data = await response.json();
-                setNote(data.text);
+                try {
+                    const response = await getNote(groupId, choosenDay, PATH, TOKEN, AUTH);
+                    const data = await response.json();
+                    setNote(data.text);
+                }
+                catch (error) {
+                    console.log('Network error: ', error)
+                }
             }
         }
 
@@ -30,7 +35,17 @@ export default function Modal({ groupId, choosenDay, setModalVision }) {
             <div className={classes.modalWindow} onClick={(event) => event.stopPropagation()}>
                 <p className={classes.noteTitle}>CLASS NOTE | {choosenDay}</p>
                 <textarea className={classes.note} value={note} onChange={(e) => setNote(e.target.value)}></textarea>
-                <div className={classes.saveCont}><div onClick={() => { setModalVision(false); postNote(groupId, choosenDay, note, PATH, TOKEN, AUTH) }} className={classes.save}>SAVE</div></div>
+                <div className={classes.saveCont}><div onClick={() => {
+                    setModalVision(false);
+                    if (groupId) {
+                        try {
+                            postNote(groupId, choosenDay, note, PATH, TOKEN, AUTH);
+                        }
+                        catch (error) {
+                            console.log('Network error: ', error)
+                        }
+                    }
+                }} className={classes.save}>SAVE</div></div>
             </div>
         </div>
     )
